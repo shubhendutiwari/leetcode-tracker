333c5ead1a582f83821e99037fe97e072e75a27e
Shubhendu Tiwari <30563065+shubhendutiwari@users.noreply.github.com>
2025-09-26 13:18:18 +0800

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 9/26/2025, 1:18:18 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        for(int right = 0; right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+            }
+        }
+    }
+}
